9ac213e05dbeed434b1b0d897aa5ce9bc37df539
Hamidreza Navaei <HRNavaei@yahoo.com>
2024-10-09 16:42:41 +0330

    Correct a typo

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const dotenv = require('dotenv');
 dotenv.config({ path: `${__dirname}/config.env` });
 
 const dbUri = process.env.DB_URI;
-mongoose.connect(dbUri).then(() => console.log('Connect to DB successfully'));
+mongoose.connect(dbUri).then(() => console.log('Connected to DB successfully'));
